2b249e78b9c301884cbff1caf1369a3f63f9bd98
nakanaka773 <hirayama773@icloud.com>
2023-11-12 06:10:07 +0900

    Initial Commit

---
+++ StudyApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StudyApp
+//
+//  Created by 平山奈々海 on 2023/11/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudyApp/StudyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyAppApp.swift
+//  StudyApp
+//
+//  Created by 平山奈々海 on 2023/11/12.
+//
+
+import SwiftUI
+
+@main
+struct StudyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
